055a6c24eb56d17720f008b92ff4c6de6f92cdba
Y0d0g MiMiMi <github@stippmilch.de>
2024-06-28 12:56:17 +0200

    updates comment

--- helpers/create-mrpe.py
+++ helpers/create-mrpe.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
-
 # create mrpe config for cluster nodes to check each other via icmp
+
+
+# Install:
+# curl --silent https://raw.githubusercontent.com/grepmeister/checkmk-helpers/main/helpers/create-mrpe.py  | python3 >> /etc/check_mk/mrpe.cfg
+
+# make check_icmp available on both nodes as /usr/local/bin/check_icmp
+# cp /opt/omd/versions/default/share/check_mk/checks/check_icmp /usr/local/bin/
+# scp /opt/omd/versions/default/share/check_mk/checks/check_icmp root@other-node:/usr/local/bin/
+
 
 my_own_ips = []
 
